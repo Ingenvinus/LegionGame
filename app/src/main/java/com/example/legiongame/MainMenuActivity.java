ce92baf529b0e97844cfb475648c257266392dc5
IvardenBraber <trolled@live.nl>
2021-10-06 11:50:44 +0200

    Main menu naar game activity
    Knop gemaakt om door te gaan naar de game activity

--- app/src/main/java/com/example/legiongame/MainMenuActivity.java
+++ app/src/main/java/com/example/legiongame/MainMenuActivity.java
@@ -1,6 +1,9 @@
 package com.example.legiongame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,15 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonStart = findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenuActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
