1815c84ba6b305a8e78ec72191061f40ee4c2453
IvardenBraber <trolled@live.nl>
2021-10-28 19:05:38 +0200

    Libgdx toegevoegd
    klasse aangemaakt waar de game in zal draaien, deze wordt als view getoond in een andere klassen.
    Alle dependencies voor libgdx toegevoegd

--- app/src/main/java/com/example/legiongame/GameActivity.java
+++ app/src/main/java/com/example/legiongame/GameActivity.java
@@ -1,14 +1,16 @@
 package com.example.legiongame;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class GameActivity extends AppCompatActivity {
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class GameActivity extends AndroidApplication {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new LegionGame(), config);
     }
 }
---
+++ app/src/main/java/com/example/legiongame/LegionGame.java
@@ -0,0 +1,30 @@
+package com.example.legiongame;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class LegionGame extends Game {
+    private static final float SCALE = 2.0f;
+    public static final float PIXEL_PER_METER = 32f;
+
+    private OrthographicCamera orthographicCamera;
+    @Override
+    public void create () {
+        orthographicCamera = new OrthographicCamera();
+        orthographicCamera.setToOrtho(false, Gdx.graphics.getWidth() / SCALE, Gdx.graphics.getHeight() / SCALE);
+    }
+    @Override
+    public void render () {
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+    @Override
+    public void resize(int width, int height) {
+        orthographicCamera.setToOrtho(false, width / SCALE, height / SCALE);
+    }
+    @Override
+    public void dispose() {
+    }
+}
