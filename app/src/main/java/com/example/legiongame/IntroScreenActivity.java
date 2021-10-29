bae09f9ee147ecf527188150afbd6508881664a4
Ingenvinus <calvindijk76@gmail.com>
2021-10-29 13:47:56 +0200

    Fullscreen Splashscreen fixed
    themes is aangepast

--- app/src/main/java/com/example/legiongame/IntroScreenActivity.java
+++ app/src/main/java/com/example/legiongame/IntroScreenActivity.java
@@ -11,7 +11,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowInsets;
+import android.view.WindowManager;
 
 import com.example.legiongame.databinding.ActivityIntroScreenBinding;
 
@@ -26,6 +28,8 @@ public class IntroScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_intro_screen);
         new Handler().postDelayed(new Runnable() {
             @Override
