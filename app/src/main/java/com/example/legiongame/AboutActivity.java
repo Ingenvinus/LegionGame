85188ab6c5655dff8995e6b9d53415176ba173a0
Ingenvinus <91918399+Ingenvinus@users.noreply.github.com>
2021-12-23 14:38:00 +0100

    youtube added
    Youtube + Icons added

--- app/src/main/java/com/example/legiongame/AboutActivity.java
+++ app/src/main/java/com/example/legiongame/AboutActivity.java
@@ -3,6 +3,8 @@ package com.example.legiongame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView YouTubeTextView = findViewById(R.id.textView10);
+        YouTubeTextView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
